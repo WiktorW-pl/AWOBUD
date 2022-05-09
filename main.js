9be307771b88c225b20b82860180aee761df6f69
WiktorW-PL <modusii.90@wp.pl>
2022-05-09 15:00:38 +0200

    Initial commit
    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const svg = document.querySelector('.services__svg');
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        svg.src = "services-mobile.svg"
+        svg.style.marginTop = 0 + "px";
+    } else {
+        svg.src = "services.svg"
+        svg.style.marginTop = 60 + "px";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 700px)")
+  myFunction(x) 
+  x.addListener(myFunction) 
